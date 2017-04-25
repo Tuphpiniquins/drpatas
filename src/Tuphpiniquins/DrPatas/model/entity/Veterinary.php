7826320a8727a27f5de87eb75978eac7e7e0427e
Rodrigo Victor Ribeiro <rribeiro@webmotors.com.br>
2017-04-25 17:39:11 -0300

    Fixing codesniffer errors and improving php7 support to Veterinary entity class

--- src/Tuphpiniquins/DrPatas/model/entity/Veterinary.php
+++ src/Tuphpiniquins/DrPatas/model/entity/Veterinary.php
@@ -1,53 +1,55 @@
 <?php
+declare(strict_types = 1);
+
+
 namespace Tuphpiniquins\Drpatas\Model\Entity;
 
 class Veterinary extends AbstractEntity
 {
-	
-	private $name;
-	private $phone;
-	private $email;
-	private $address;
-	private $services;
+    
+    private $name;
+    private $phone;
+    private $email;
+    private $address;
+    private $services;
 
-	public function __construct(string $name, Phone $phone, string $email, Address $address, array $services)
-	{
-		$this->name = $name;
-		$this->phone = $phone;
-		$this->email = $email;
-		$this->address = $address;
-		$this->services = $services;
+    public function __construct(string $name, Phone $phone, string $email, Address $address, array $services)
+    {
+        $this->name = $name;
+        $this->phone = $phone;
+        $this->email = $email;
+        $this->address = $address;
+        $this->services = $services;
 
-		$this->validate();
-	}
+        $this->validate();
+    }
 
-	public function getName()
-	{
-		return $this->name;
-	}
+    public function getName() : string
+    {
+        return $this->name;
+    }
 
-	public function getPhone()
-	{
-		return $this->phone;
-	}
+    public function getPhone() : Phone
+    {
+        return $this->phone;
+    }
 
-	public function getEmail()
-	{
-		return $this->email;
-	}
+    public function getEmail() : string
+    {
+        return $this->email;
+    }
 
-	public function getAddress()
-	{
-		return $this->address;
-	}
+    public function getAddress() : Address
+    {
+        return $this->address;
+    }
 
-	public function getServices()
-	{
-		return $this->services;
-	}
+    public function getServices() : array
+    {
+        return $this->services;
+    }
 
-	public function validate()
-	{
-
-	}
+    public function validate()
+    {
+    }
 }
